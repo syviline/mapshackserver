43f10f35edc43757331c3cf37d4869f5b32602d1
magnit <GeorgeGoldobin@yandex.ru>
2020-12-26 18:22:26 +0500

    Version 4

--- jsonPost.py
+++ jsonPost.py
@@ -5,22 +5,24 @@ URL = url = 'http://127.0.0.1:5000'
 # URL = "https://mapshack.herokuapp.com/"
 CITY = "Тюмень"
 
-_markAll = "/api/shop/mark/all" # id: shopID
-_markAdd = "/api/shop/mark/put" # 
-_markGet = "/api/mark/get" # id: markID
-_markDel = "/api/mark/del" # id: markID
+_markAll = "/api/shop/mark/all"  # id: shopID
+_markAdd = "/api/shop/mark/put"  #
+_markGet = "/api/mark/get"  # id: markID
+_markDel = "/api/mark/del"  # id: markID
 
-_shopAll = "/api/shop/all" 
+_shopAll = "/api/shop/all"
 _shopAdd = "/api/shop/put"
-_shopGet = "/api/shop/get" # id: shopID
-_shopDel = "/api/shop/del" # id: shopID
+_shopGet = "/api/shop/get"  # id: shopID
+_shopDel = "/api/shop/del"  # id: shopID
 
-_cityAll = "/api/city/all" 
+_cityAll = "/api/city/all"
+
 
 def reqePost(url, command, data={}):
     answer = requests.post(url + command, data=json.dumps(data))
     response = answer.json()
-    return response 
+    return response
+
 
 def addMark(shopID, name, descript, lat, lng, markID=None):
     data = {"shopID": shopID, "name": name,
@@ -39,8 +41,9 @@ def addShop(ID, name, descript, lat, lng, website="NULL", image="NULL", marks=[]
             "position": {"lat": lat, "lng": lng},
             "marks": marks,
             "city": city}
-    
+
     return reqePost(URL, _shopAdd, data)
+
 
 data = {"id": "9-9-6"}
 dataM = {"id": "1-7"}
@@ -51,18 +54,17 @@ input()
 print(_shopAdd, addShop("9-9-6", "Монеточка",
                         "Магазин продуктов", 12.1, 46.34,
                         "monetka.ru", image="nulllll",
-                        city = CITY,
-                        marks = [{'name': 'Холодильник', #'id': '13',
-                   'description': 'Холодильник с молоком',
-                   'position': {'lat': 12.12, 'lng': 32.31},
-                                  'id': '1-7'}]), sep="\n")
+                        city=CITY,
+                        marks=[{'name': 'Холодильник',  # 'id': '13',
+                                'description': 'Холодильник с молоком',
+                                'position': {'lat': 12.12, 'lng': 32.31},
+                                'id': '1-7'}]), sep="\n")
 input()
 print(_shopAll, reqePost(URL, _shopAll), sep="\n")
 input()
 
 print(_shopGet, reqePost(URL, _shopGet, {"id": "9-9-9"}), sep="\n")
 input()
-
 
 print(_markAdd, addMark("9-9-6", "Полка", "Полка шеколадками2", 12.1, 46.34, "1-7"), sep="\n")
 input()
